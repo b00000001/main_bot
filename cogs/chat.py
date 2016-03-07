98bd90b34f56d1764f43ee4f5a68a84f3e4ac847
redfruitt <krishnaqmar@gmail.com>
2016-03-07 23:47:41 +0530

    added chat cog

---
+++ cogs/chat.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+import datetime
+import time
+import aiohttp
+import asyncio
+
+""" Chat cog """
+class Chat:
+	
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(hidden= True)
+	async def say(self, *text):     # !say text
+	     
+	     await self.bot.say(" ".join(text))
+        
+
+
+
+
+
+
+
+
+def setup(bot):
+   
+    bot.add_cog(Chat(bot))
