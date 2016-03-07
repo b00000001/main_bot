68b53e9d3ce29ca199caf6b579761fd4f434a612
OnyxChills <brandonturmel@gmail.com>
2016-03-07 15:27:38 -0500

    Cleaned up some comments.

--- cogs/reddit_scraper.py
+++ cogs/reddit_scraper.py
@@ -3,18 +3,11 @@ import discord, asyncio
 from discord.ext import commands
 from .utils import checks, scraper
 
-
-
 #this is the cog / extension
 class RedditScraper:
 
     def __init__(self, bot : commands.Bot):
         self.bot = bot
-
-    #hidden = True means that when you do [command bang] help it won't show
-    # @commands.command(hidden=True)
-    # async def test(self, *message):
-    #     await self.bot.say(" ".join(message))
 
     @commands.group(name="reddit", pass_context=True)
     @checks.is_owner()
@@ -31,35 +24,9 @@ class RedditScraper:
         else:
             if subreddit.lower().strip().__len__() > 0:
                 result = await scraper.getSubredditTop(self.bot.session, subreddit, posts)
-                # i = 0
-                # for post in result:
-                #     await self.bot.say(post) if i == 0 else await self.bot.say('\n' + post)
-                #     asyncio.sleep(1)
                 await self.bot.say("\n\n".join(result))
             else:
                 await self.bot.pm_help(ctx)
-
-    # #pass_context will pass on the message info of the person who called the command
-    # #ctx is needed for the pass_context
-    # #num is a sub-command thing like !clearbot 29
-    # @commands.command(pass_context=True)
-    # async def clearbot(self, ctx, num: int):
-    #     count = 0
-    #     if num < 0:
-    #         return
-    #
-    #     while count <= num:
-    #         del_flag = False
-    #         async for msg in self.bot.logs_from(ctx.message.channel, limit=100):
-    #             if count == num:
-    #                 break
-    #             elif msg.author.id == self.bot.user.id:
-    #                 count += 1
-    #                 del_flag = True
-    #                 await self.bot.delete_message(msg)
-    #
-    #         if not del_flag:
-    #             break
 
 
 #for the bot to add the cog. replace template with the classname
